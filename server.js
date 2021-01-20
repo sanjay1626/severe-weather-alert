1686cb89da3923b92aecace3040b558213474187
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-01-19 23:20:09 -0800

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser')
 var env = require('dotenv')
 var exphbs = require('express-handlebars')
  
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 //For BodyParser
 app.use(bodyParser.urlencoded({
     extended: true
